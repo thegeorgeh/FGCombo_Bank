7a7779208359b9562daecafc86027715747b916d
GeorgeH_games <georgeh4@live.co.uk>
2019-03-03 11:45:25 +0000

    initial commit

---
+++ FGComboBank_BackEnd/src/main/java/com/qa/InProject/FgComboBankBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.qa.InProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgComboBankBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgComboBankBackEndApplication.class, args);
+	}
+
+}
---
+++ FGComboBank_BackEnd/src/test/java/com/qa/InProject/FgComboBankBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qa.InProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FgComboBankBackEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
